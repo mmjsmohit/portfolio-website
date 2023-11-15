3553b894ca6d98845a13a0b113e2f9a83e1a2874
mmjsmohit <mmjsmohit@gmail.com>
2023-11-15 10:52:01 +0530

    Update metadata, styles, and dependencies

--- components/intro.tsx
+++ components/intro.tsx
@@ -6,9 +6,12 @@ import { motion } from "framer-motion";
 import Link from "next/link";
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
-import { FaGithubSquare } from "react-icons/fa";
+import { FaGithubSquare, FaTwitterSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
+import profile from "@/public/profile.jpg";
 import { useActiveSectionContext } from "@/context/active-section-context";
+
+
 
 export default function Intro() {
   const { ref } = useSectionInView("Home", 0.5);
@@ -21,7 +24,7 @@ export default function Intro() {
       className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
     >
       <div className="flex items-center justify-center">
-        <div className="relative">
+        <div className="relative" style={{ transform: 'scale(1.2)' }}>
           <motion.div
             initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
@@ -31,7 +34,7 @@ export default function Intro() {
             }}
           >
             <Image
-              src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
+              src={profile}
               alt="Ricardo portrait"
               width="192"
               height="192"
@@ -62,11 +65,10 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{" "}
-        <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">8 years</span> of experience. I enjoy
+        <span className="font-bold">Hello, I'm Mohit.</span> I'm a{" "}
+        <span className="font-bold">creative-mobile developer</span> . I enjoy 
         building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        <span className="underline">Flutter and Dart</span>.
       </motion.h1>
 
       <motion.div
@@ -100,7 +102,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com"
+          href="https://www.linkedin.com/in/mohit-tiwari-linked-in/"
           target="_blank"
         >
           <BsLinkedin />
@@ -108,10 +110,17 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com"
+          href="https://github.com/mmjsmohit"
           target="_blank"
         >
           <FaGithubSquare />
+        </a>
+        <a
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://twitter.com/MohitTiwari2003"
+          target="_blank"
+        >
+          <FaTwitterSquare />
         </a>
       </motion.div>
     </section>
